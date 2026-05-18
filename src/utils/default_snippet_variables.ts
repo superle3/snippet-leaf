f919cc0abef971c03c2a3dd2d052dadcf578b9a4
superle3 <987654321.yo.na.than@gmail.com>
2026-05-18 23:36:41 +0200

    ci: only update settings bundle when needed
    The settings bundle wont get updated every release thus to make
    communication clear to the user when it has updated and thus new
    settings might need to be added, the version is increased.

--- src/utils/default_snippet_variables.ts
+++ src/utils/default_snippet_variables.ts
@@ -2,7 +2,7 @@
 import default_snippet_variables_str from "inline:src/default_snippet_variables.json5";
 import type { SnippetVariables } from "src/extension";
 // @ts-ignore
-import default_snippet_variables_json from "../default_snippet_variables.json5";
+import default_snippet_variables_json from "src/default_snippet_variables.json5";
 
 export const DEFAULT_SNIPPET_VARIABLES_str: string =
     default_snippet_variables_str;
